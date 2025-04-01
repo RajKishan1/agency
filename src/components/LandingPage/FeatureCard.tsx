c34e349630ee6dec0844d853e571220b1345ea28
Raj Kishan Verma <rajkishanvrma@gmail.com>
2025-04-01 14:40:36 +0530

    fix:cards

--- src/components/LandingPage/FeatureCard.tsx
+++ src/components/LandingPage/FeatureCard.tsx
@@ -9,19 +9,19 @@ interface prop {
 const FeatureCard = ({ bgcolor, src, text }: prop) => {
   return (
     <div
-      className={`w-[48%] ${bgcolor} flex gap-5 p-12 rounded-4xl border-[1px] border-black`}
+      className={`w-1/2 max-w-[600px] h-[310px] ${bgcolor} flex gap-5 p-12 rounded-4xl border-[1px] border-black shadow-[10px] shadow-black`}
     >
       <div className="w-1/2 flex flex-col justify-between">
-        <h2 className="bg-green-200">{text}</h2>
-        <div className="flex">
-          <span className="rounded-full">
-            <ArrowRight />
+        <h2 className="bg-[#b9ff66] w-auto text-3xl font-semibold">{text}</h2>
+        <div className="flex items-center text-xl gap-2">
+          <span className="rounded-full bg-white p-2 -rotate-45">
+            <ArrowRight size={30} />
           </span>
           Learn more
         </div>
       </div>
       <div className="w-1/2">
-        <Image src={src} height={200} width={200} alt="" />
+        <Image src={src} height={250} width={250} alt="" />
       </div>
     </div>
   );
--- src/components/LandingPage/Features.tsx
+++ src/components/LandingPage/Features.tsx
@@ -3,18 +3,42 @@ import FeatureCard from "./FeatureCard";
 
 const Features = () => {
   return (
-    <div className="flex flex-col  justify-between gap-12">
+    <div className="flex flex-col  justify-around gap-12">
       <span className="flex justify-between">
-        <FeatureCard text=" lorem ipsum" src="" bgcolor="bg-[#f3f3f3]" />
-        <FeatureCard text=" lorem ipsum" src="" bgcolor="bg-[#b9ff66]" />
+        <FeatureCard
+          text="Search Engine Optimization"
+          src="/images/magnifier.png"
+          bgcolor="bg-[#f3f3f3]"
+        />
+        <FeatureCard
+          text=" Pay-per-click Advertising"
+          src="/images/arrow.png"
+          bgcolor="bg-[#b9ff66]"
+        />
       </span>
       <span className="flex justify-between">
-        <FeatureCard text=" lorem ipsum" src="" bgcolor="bg-[#191a23]" />
-        <FeatureCard text=" lorem ipsum" src="" bgcolor="bg-[#f3f3f3]" />
+        <FeatureCard
+          text="Social Media Marketing"
+          src="/images/smile.png"
+          bgcolor="bg-[#191a23]"
+        />
+        <FeatureCard
+          text="Email Marketing"
+          src="/images/sending.png"
+          bgcolor="bg-[#f3f3f3]"
+        />
       </span>
       <span className="flex justify-between">
-        <FeatureCard text=" lorem ipsum" src="" bgcolor="bg-[#b9ff66]" />
-        <FeatureCard text=" lorem ipsum" src="" bgcolor="bg-[#191a23]" />
+        <FeatureCard
+          text="Content Creation"
+          src="/images/tasks.png"
+          bgcolor="bg-[#b9ff66]"
+        />
+        <FeatureCard
+          text="Analytics and Tracking"
+          src="/images/charts.png"
+          bgcolor="bg-[#191a23]"
+        />
       </span>
     </div>
   );
